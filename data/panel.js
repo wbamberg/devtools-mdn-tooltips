d083b2730cb73324931dfeff6065173455575bef
Will Bamberg <wbamberg@mozilla.com>
2014-02-21 11:16:16 -0800

    include example in main pane

--- data/panel.js
+++ data/panel.js
@@ -20,32 +20,9 @@ self.on("message", function(message) {
     highlights[i].classList.add("theme-fg-color5");
   }
 
-  handleOverviewClick();
-
-  let overview = document.getElementById("overview-button");
-  overview.addEventListener("click", handleOverviewClick, false);
-
-  let example = document.getElementById("example-button");
-  example.addEventListener("click", handleExampleClick, false);
-
   let visitPage = document.getElementById("visit-page-button");
   visitPage.addEventListener("click", handleVisitPageClick, false);
 });
-
-function handleOverviewClick() {
-  let overview = document.getElementById("overview");
-  overview.style.display = "block";
-  let example = document.getElementById("example");
-  example.style.display = "none";
-}
-
-function handleExampleClick() {
-  console.log("clicked");
-  let overview = document.getElementById("overview");
-  overview.style.display = "none";
-  let example = document.getElementById("example");
-  example.style.display = "block";
-}
 
 function handleVisitPageClick(e) {
   e.stopPropagation();
